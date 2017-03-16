34bf342b219c67d5103e66c4e97ea6319e0b6793
Ye Wang <Ye Wang>
2017-03-16 13:59:11 -0700

    Initial commit

---
+++ app/src/main/java/com/example/wangy/claycount/Event2.java
@@ -0,0 +1,13 @@
+package com.example.wangy.claycount;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class Event2 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_event2);
+    }
+}
---
+++ app/src/main/java/com/example/wangy/claycount/EventHome.java
@@ -0,0 +1,13 @@
+package com.example.wangy.claycount;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class EventHome extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_event_home);
+    }
+}
